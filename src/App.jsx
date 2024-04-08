2b68196269bf399de3f58cebfb85e5df43e22663
Robert Lauv <robert.lauv3@gmail.com>
2024-04-07 18:44:45 -0700

    Added line chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,48 @@
 import './App.css';
 // import {Bargraph} from './components';
 import {BarChart} from '@mui/x-charts/BarChart';
-import {FormGroup} from '@mui/material';
-import {FormControlLabel} from '@mui/material';
-import {Checkbox} from '@mui/material';
+import {LineChart} from '@mui/x-charts/LineChart';
+import {FormGroup, FormControlLabel, Checkbox} from '@mui/material';
 
-let dataset = [
-  {
-    metric: "Deployment Frequency",
-    negative_correlation: 0.3
-  },
-  {
-    metric: "Open Issue Bug Count",
-    positive_correlation: 0.5
-  },
-  {
-    metric: "Average Pull Request Pickup Time",
-    negative_correlation: 0.6
-  },
-  {
-    metric: "Retrospective Meeting Satisfaction",
-    negative_correlation: 0.2
-  },
-  {
-    metric: "Mid-Sprint Refinement Changes",
-    positive_correlation: 0.8
-  }
+let raw_data = {
+  'Deployment Frequency': [15, 12, 18, 20, 17, 14, 22, 19, 16, 21],
+  'Lead Time for Changes': [3, 4, 2, 5, 3, 4, 6, 2, 3, 5],
+  'Change Failure Rate': [0.1, 0.2, 0.05, 0.15, 0.1, 0.25, 0.2, 0.1, 0.15, 0.05],
+  'Time to Restore Service': [4, 6, 2, 5, 4, 3, 7, 4, 3, 5],
+  'Open Issue Bug Count': [25, 30, 20, 22, 28, 26, 18, 24, 29, 23],
+  'Pull Request Pickup Time': [2, 1, 3, 2, 1, 2, 4, 3, 2, 1],
+  'Refinement Changes': [10, 12, 8, 9, 11, 15, 7, 10, 8, 13],
+  'Task Block Time': [2, 3, 1, 2, 3, 4, 2, 1, 2, 3],
+  'Meeting Satisfaction': [4, 3, 5, 4, 3, 5, 4, 3, 5, 4]
+}
+
+// function toJSON(data){
+//   let ret = []
+//   for (let key in data){
+//     ret.push({metric: key, values: data[key]})
+//   }
+//   return ret;
+// }
+
+// raw_data = toJSON(raw_data);
+
+let correlations = [
+  ['Deployment Frequency',       -0.409392],
+  ['Lead Time for Changes',       0.429082],
+  ['Change Failure Rate',         0.516650],
+  ['Time to Restore Service',     0.164581],
+  ['Open Issue Bug Count',        0.466147],
+  ['Pull Request Pickup Time',   -0.624221],
+  ['Refinement Changes',          0.802181],
+  ['Meeting Satisfaction',        0.000000]
 ]
+
+let dataset = correlations.map((metric) => {
+  if ( metric[1] < 0 ){
+    return {metric: metric[0], negative_correlation: -metric[1]}
+  }
+  return {metric: metric[0], positive_correlation: metric[1]}
+});
 
 dataset = dataset.sort((a, b) => {
   let aval = a.hasOwnProperty('positive_correlation') ? a.positive_correlation : a.negative_correlation;
@@ -43,18 +59,36 @@ const checkboxes = dataset.map((data) => {
   return <FormControlLabel control={<Checkbox />} label={data.metric} />
 })
 
+// [`Deployment Frequency`, 
+//                 `Lead Time for Changes`, 
+//                 `Change Failure Rate`, 
+//                 `Time to Restore Service`, 
+//                 `Open Issue Bug Count`, 
+//                 `Pull Request Pickup Time`, 
+//                 `Refinement Changes`, 
+//                 `Task Block Time`, 
+//                 `Meeting Satisfaction`]
+
+const include = [`Refinement Changes`, 
+                `Task Block Time`]
+
+const get_series = include.map((metric) => {
+  return {label: metric, data: raw_data[metric]}
+})
+
 function App() {
   return (
-    <div class="chart-container">
+    <>
+    <div class="bar-chart-container">
       <div class="chart">
         <h1>Compare Against...</h1>
         <FormGroup>
-        <FormControlLabel control={<Checkbox />} label="Rollover Rate" />
         {checkboxes}
+        <FormControlLabel control={<Checkbox />} label="Average Task Blocked Time" />
         </FormGroup>
       </div>
       <div class="chart">
-        <p>Rollover Rate vs. Metrics</p>
+        <p>Correlation Against: Average Task Blocked Time</p>
         <BarChart
           dataset={dataset}
           yAxis={[{ scaleType: 'band', dataKey: 'metric' }]}
@@ -67,6 +101,26 @@ function App() {
         />
       </div>
     </div>
+    <div class="line-chart-container">
+      <div class="chart">
+        <h1>Check Metrics</h1>
+        <FormGroup>
+        {checkboxes}
+        <FormControlLabel control={<Checkbox />} label="Average Task Blocked Time" />
+        </FormGroup>
+      </div>
+      <div class="chart">
+        <p>Raw Data</p>
+        <LineChart
+          xAxis={[{data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], label: 'Sprint Number'}]}
+          series={get_series}
+          layout="horizontal"
+          height={600}
+          width={600}
+        />
+      </div>
+    </div>
+    </>
   )
 }
 

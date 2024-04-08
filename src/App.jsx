66219114d0ad6af5613a9908ec69519fa1e680ac
Robert Lauv <robert.lauv3@gmail.com>
2024-04-07 17:45:35 -0700

    Add checkboxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import './App.css'
+import './App.css';
 // import {Bargraph} from './components';
-import {BarChart} from '@mui/x-charts/BarChart'
+import {BarChart} from '@mui/x-charts/BarChart';
+import {FormGroup} from '@mui/material';
+import {FormControlLabel} from '@mui/material';
+import {Checkbox} from '@mui/material';
 
 let dataset = [
   {
@@ -31,27 +34,39 @@ dataset = dataset.sort((a, b) => {
   return aval < bval? 1 : -1;
 })
 
-const positive_color = '#0057D2';
-const negative_color = '#AA0808';
+// const positive_color = '#0057D2';
+// const negative_color = '#AA0808';
+const positive_color = '#AA0808';
+const negative_color = '#0057D2';
+
+const checkboxes = dataset.map((data) => {
+  return <FormControlLabel control={<Checkbox />} label={data.metric} />
+})
 
 function App() {
-
   return (
-    <>
-    <div>
-      <p>Rollover Rate vs. Metrics</p>
-      <BarChart
-        dataset={dataset}
-        yAxis={[{ scaleType: 'band', dataKey: 'metric' }]}
-        series={[{ dataKey: 'positive_correlation', label: 'Positive Correlation', color: positive_color, stack: 'total'},
-                { dataKey: 'negative_correlation', label: 'Negative Correlation', color: negative_color, stack: 'total'}]}
-        layout="horizontal"
-        height={400}
-        width={600}
-        margin={{left: 300}}
-      />
+    <div class="chart-container">
+      <div class="chart">
+        <h1>Compare Against...</h1>
+        <FormGroup>
+        <FormControlLabel control={<Checkbox />} label="Rollover Rate" />
+        {checkboxes}
+        </FormGroup>
+      </div>
+      <div class="chart">
+        <p>Rollover Rate vs. Metrics</p>
+        <BarChart
+          dataset={dataset}
+          yAxis={[{ scaleType: 'band', dataKey: 'metric' }]}
+          series={[{ dataKey: 'positive_correlation', label: 'Positive Correlation', color: positive_color, stack: 'total'},
+                  { dataKey: 'negative_correlation', label: 'Negative Correlation', color: negative_color, stack: 'total'}]}
+          layout="horizontal"
+          height={400}
+          width={600}
+          margin={{left: 300}}
+        />
+      </div>
     </div>
-    </>
   )
 }
 

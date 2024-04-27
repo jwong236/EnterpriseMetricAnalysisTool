bfe5c8ca4fa2f45ef99b44dcc7fa78abb3cf9d4f
Robert Lauv <robert.lauv3@gmail.com>
2024-04-26 23:57:30 -0700

    Edit homepage and base for GitHub Actions

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Enterprise-Continuous-Improvement-Insights-Mockup/',
+  base: '/UCI-SAP-Capstone/',
   plugins: [react()],
 })

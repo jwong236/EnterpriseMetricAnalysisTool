f05cbcf57d3004f9bf184fdbc20d50def99c550a
Jacob Wong <jacobsunsetbluff@gmail.com>
2024-05-22 16:44:52 -0700

    Update script to include all teams

--- backend/scripts/extract_csv.py
+++ backend/scripts/extract_csv.py
@@ -1,6 +1,6 @@
 import csv
 
-def filter_csv(input_file_name, output_file_name, team, services):
+def filter_csv(input_file_name, output_file_name, services):
     with open(input_file_name, mode='r') as input_file, open(output_file_name, mode='w', newline='') as output_file:
         reader = csv.DictReader(input_file)
         fieldnames = reader.fieldnames
@@ -9,11 +9,10 @@ def filter_csv(input_file_name, output_file_name, team, services):
         writer.writeheader()
         for row in reader:
             service_name = row['Service'].split('--')[0].strip().lower()
-            if row['Team'] == team and service_name in services:
+            if service_name in services:
                 writer.writerow(row)
 
 def main():
-    team = 'itcm'
     services = {
         'agreement-search',
         'agreement-masterdata',
@@ -28,8 +27,8 @@ def main():
         'claims backend'
     }
 
-    filter_csv('original_full_DF.csv', 'filtered_DF.csv', team, services)
-    filter_csv('original_full_LTFC.csv', 'filtered_LTFC.csv', team, services)
+    filter_csv('original_full_DF.csv', 'filtered_DF.csv', services)
+    filter_csv('original_full_LTFC.csv', 'filtered_LTFC.csv', services)
 
 if __name__ == "__main__":
     main()
